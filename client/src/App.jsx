bcd4d08567f245915f47f3830559081afdd74699
annapgsm <anna.pingsmann@gmail.com>
2026-04-02 17:35:24 +0200

    feat: Improve UI of search and filter to match other css styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -275,12 +275,14 @@ function App() {
         <div className="filters">
           <input
             type="text"
+            className="filters-input"
             placeholder="Search company or title"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
 
           <select
+            className="filters-select"
             value={statusFilter}
             onChange={(e) => setStatusFilter(e.target.value)}
           >
